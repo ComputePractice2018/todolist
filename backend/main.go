ddca825a87082d69208c855e72cb670ad43effe6
Mikhail Lazarev <l.misha1708@gmail.com>
2018-07-06 04:24:11 +0300

    Создана простая функция на Golang

--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,12 @@ package main
 
 import "fmt"
 
+//GetNameAndTeam возвращает приветствие участнику команды
+func GetNameAndTeam(Name, Team string) string {
+	return fmt.Sprintf("Hello, %s, from %s team", Name, Team)
+}
+
 func main() {
 	fmt.Println("Hello World")
-	fmt.Printf("Hello, %s", "Mikhail")
+	fmt.Printf(GetNameAndTeam("Mikhail", "Todolist"))
 }
