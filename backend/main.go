42a8fd8cf82247606bdda2edbec0aa50795eec05
Mikhail Lazarev <l.misha1708@gmail.com>
2018-07-10 01:30:04 +0300

    Рефакторинг в Handlers Задача #28

--- backend/main.go
+++ backend/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gorilla/mux"
 
+	"github.com/ComputePractice2018/todolist/backend/data"
 	"github.com/ComputePractice2018/todolist/backend/server"
 )
 
@@ -13,14 +14,14 @@ func main() {
 	/*	var name = flag.String("name", "Mikhail", "имя участника команды")
 		var team = flag.String("team", "Todolist", "название команды")*/
 	//flag.Parse()
-
+	listTask := data.NewListTask()
 	router := mux.NewRouter()
 	//роутинг списка задач
-	router.HandleFunc("/api/todolist/task/", server.GetList).Methods("GET")
-	router.HandleFunc("/api/todolist/task/", server.AddList).Methods("POST")
-	router.HandleFunc("/api/todolist/task/{id}", server.EditList).Methods("PUT")
-	router.HandleFunc("/api/todolist/task/{id}", server.DeleteList).Methods("DELETE")
-	router.HandleFunc("/api/todolist/task/complete/{id}", server.CompleteList).Methods("PUT")
+	router.HandleFunc("/api/todolist/task/", server.GetList(listTask)).Methods("GET")
+	router.HandleFunc("/api/todolist/task/", server.AddList(listTask)).Methods("POST")
+	router.HandleFunc("/api/todolist/task/{id}", server.EditList(listTask)).Methods("PUT")
+	router.HandleFunc("/api/todolist/task/{id}", server.DeleteList(listTask)).Methods("DELETE")
+	router.HandleFunc("/api/todolist/task/complete/{id}", server.CompleteList(listTask)).Methods("PUT")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
