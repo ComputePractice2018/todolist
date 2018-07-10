432a476f7d8835e7feddcdda92d6dfe99bfe07ae
Mikhail Lazarev <l.misha1708@gmail.com>
2018-07-10 03:10:51 +0300

    Протестирован и отрефакторен сервер Задача #28

--- backend/main.go
+++ backend/main.go
@@ -4,8 +4,6 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
-
 	"github.com/ComputePractice2018/todolist/backend/data"
 	"github.com/ComputePractice2018/todolist/backend/server"
 )
@@ -15,13 +13,6 @@ func main() {
 		var team = flag.String("team", "Todolist", "название команды")*/
 	//flag.Parse()
 	listTask := data.NewListTask()
-	router := mux.NewRouter()
-	//роутинг списка задач
-	router.HandleFunc("/api/todolist/task/", server.GetList(listTask)).Methods("GET")
-	router.HandleFunc("/api/todolist/task/", server.AddList(listTask)).Methods("POST")
-	router.HandleFunc("/api/todolist/task/{id}", server.EditList(listTask)).Methods("PUT")
-	router.HandleFunc("/api/todolist/task/{id}", server.DeleteList(listTask)).Methods("DELETE")
-	router.HandleFunc("/api/todolist/task/complete/{id}", server.CompleteList(listTask)).Methods("PUT")
-
+	router := server.NewRouter(listTask)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
